f6a4671ff738d4dd35c6a66259de7342d6be4bce
diarmov <56406028+diarmov@users.noreply.github.com>
2026-04-15 12:54:21 -0600

    Update denuncias.blade.php

--- resources/views/principales/denuncias.blade.php
+++ resources/views/principales/denuncias.blade.php
@@ -7,7 +7,7 @@
     <div class="text-center">
         <p>
             <a href="https://sidec.zacatecas.gob.mx/" target="_blank"><img src="{{ asset('storage/imgs/sidec.jpg') }}"
-                    alt="Sistema de Atención Ciudadana" width="100%" class="rounded shadow bg-body-tertiary zoom"></a>
+                    alt="Sistema de Atención Ciudadana" width="100%" class="rounded shadow bg-body-tertiary"></a>
         </p>
         <p>
         <h5 class="alert alert-primary">El Sistema de Denuncia Ciudadana (SIDEC) del Estado de Zacatecas, es el medio
