e54e8479f0287043d03a33c49a42287d7d7d67d8
Diego <diarmov@hotmail.com>
2026-06-23 08:55:24 -0600

    Se agrega icono de sistema en pestana

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{ asset('storage/imgs/SICS.ico') }}">
 
     <style>
         :root {
